5e77b4fd18999d5f9ba1af3bd348fe060debe5d4
Jan Górecki <gorecki@opf.slu.cz>
2024-09-26 15:41:54 +0200

    simple exps

---
+++ papers/gradient_journal/dev/experiments_simple.py
@@ -0,0 +1,243 @@
+from pathlib import Path
+import itertools
+from multiprocessing import Pool, cpu_count
+import os
+import glob
+from collections import defaultdict
+import json
+
+from concurrent_log_handler import ConcurrentRotatingFileHandler
+#pip install concurrent-log-handler
+
+import logging
+import sys
+
+import pandas as pd
+
+from bcause.inference.causal.multi import GDCC, EMCC
+from bcause.models.cmodel import StructuralCausalModel
+from bcause.util import randomUtil
+from bcause.util.mathutils import rrmse, rmse
+from bcause.util.runningutils import get_logger
+from bcause.util.watch import Watch
+
+
+### Set parameters ###
+
+# Single parameters
+num_runs = 100
+run_step = 5
+resfolder = "./papers/gradient_journal/results/synthetic/simple/"
+rewrite = True
+
+# Multi parameters
+USE_FULL_PARAMETERS = True
+if USE_FULL_PARAMETERS:
+    seed_values = [1] # after our discussion, we keep only one value for the moment
+    remove_outliers_values = [True, False]
+    method_values = ["EMCC", "GDCC"]
+    max_iter_values_emcc = [25, 50, 100, 150, 200]  # Relevant for EMCC
+    tol_values_gdcc = [10 ** -i for i in range(1,11)]     # Relevant for GDCC
+else: # subset of full parameters used for debugg
+    seed_values = [1]
+    remove_outliers_values = [True, False]
+    method_values = ["GDCC"] #["EMCC", "GDCC"]
+    max_iter_values_emcc = [25, 50, 100, 150]  # Relevant for EMCC
+    tol_values_gdcc = [1e-3, 1e-5, 1e-7, 1e-9]  # Relevant for GDCC
+    # this settings renders 16 independent process, as we have 16 available workers at our machine
+
+### Set parameters End ###
+
+log_format = '%(asctime)s|%(levelname)s|%(filename)s: %(message)s'
+logfile = Path(Path(resfolder).parent, f"experiments.log")
+
+
+def get_clogger(logname, level=logging.INFO, stream=sys.stdout, fmt=None, filename=None):
+    # ConcurrentRotatingFileHandler
+    log = logging.getLogger(logname)
+    log.setLevel(level)
+    stdout_handler = logging.StreamHandler(stream)
+    if fmt is not None:
+        stdout_handler.setFormatter(logging.Formatter(fmt))
+    log.addHandler(stdout_handler)
+
+    if filename is not None:
+        # Use ConcurrentRotatingFileHandler instead of FileHandler
+        file_handler = ConcurrentRotatingFileHandler(filename, "a", 512*1024, 5)
+        if fmt is not None:
+            file_handler.setFormatter(logging.Formatter(fmt))
+        log.addHandler(file_handler)
+    return log
+
+
+def process_parameters(params, log):
+    num_runs, modelpath, resfolder, run_step, seed, remove_outliers, method, max_iter, tol = params
+    # Processing logic here
+    print(f"Processing: {seed=}, {remove_outliers=}, {method=}, {max_iter=}, {tol=}") # Single parameters are omitted here
+
+    # Set the random seed
+    randomUtil.seed(seed)
+
+    # Load the model
+    model = StructuralCausalModel.read(modelpath)
+    if 0:
+        # plot the model
+        import networkx as nx
+        import matplotlib.pyplot as plt
+        nx.draw(model.graph, with_labels=True, font_weight='bold')
+        plt.show() 
+    #model.factors["V5"].values
+
+    # Load data
+    data = pd.read_csv(modelpath.replace(".uai",".csv"), index_col=0)
+    data = data.rename(columns={c:"V"+c for c in data.columns})
+
+    # Load the information about the query and the model
+    modelpath_ccve = modelpath.replace(".uai","_query.csv")
+    if os.path.exists(modelpath_ccve):
+        info_query = pd.read_csv(modelpath_ccve)
+    else:
+        print(f'{modelpath_ccve} does not exists, skipping ...') # JAN2RAFA: some of these modelpath_ccve were missing, so I skipped them
+        return
+    
+    nV = int(len(info_query) / 2)
+    causes = [f'V{i}' for i in range(1, nV + 1)] # V1, V2, V3
+    effect = 'V0'
+
+    p_types = ['PS', 'PN'] # 'PNS'
+    exact = defaultdict(lambda : {})
+    for cause in causes:
+        for p_type in p_types:
+            interval = (info_query[(info_query['cause'] == cause) & (info_query['query'] == p_type)]['low'].values[0],
+                        info_query[(info_query['cause'] == cause) & (info_query['query'] == p_type)]['upp'].values[0])
+            exact[cause][p_type] = interval
+   
+    modelname = os.path.basename(modelpath).split(".")[0]
+
+
+    # Set the results
+    resfilepath = Path(resfolder, f"{modelname}_uai_{method}_x{num_runs}_iter{max_iter}_tol{tol}_ro{remove_outliers}_s{seed}.csv")
+    results = pd.DataFrame()
+
+    # Check that the experiments with these parameters have not been run yet
+    # if resfilepath.exists() and not rewrite:
+    #     return
+
+    # Determine the method
+    if method == "GDCC":
+        inf = GDCC(model, data, num_runs=num_runs, tol = tol, outliers_removal=remove_outliers)
+    elif method == "EMCC":
+        inf = EMCC(model, data, num_runs=num_runs, max_iter=max_iter, outliers_removal=remove_outliers)
+    else:
+        raise ValueError("Wrong learning method")
+
+    ### Start processing ###
+    tlearn = 0
+    t0 = 0
+    Watch.start()
+
+    # Learning loop
+    for _ in inf.compile_incremental(run_step): # The learning is done here at each iteration
+
+        t1 = Watch.get_time()
+        # Run the queries
+        approx = defaultdict(lambda : {})
+        for cause in causes:
+            for p_type in p_types:
+                if p_type == 'PNS':
+                    p = inf.prob_necessity_sufficiency(cause, effect, true_false_cause=(0,1), true_false_effect=(0,1))
+                elif p_type == 'PS':
+                    p = inf.prob_sufficiency(cause, effect, true_false_cause=(0,1), true_false_effect=(0,1))
+                elif p_type == 'PN':
+                    p = inf.prob_necessity(cause, effect, true_false_cause=(0,1), true_false_effect=(0,1))
+                approx[cause][p_type] = p
+        t2 = Watch.get_time()
+
+        tlearn = tlearn + t1-t0 
+        tinfer = t2-t1
+        # NOTE: When `compile_incremental()` is called, learning occurs only for the new iterations. 
+        #       However, when `inf.prob_necessity_sufficiency` is called, inference is indeed performed again with all the learned models.
+
+        err, err2 = defaultdict(lambda : {}), defaultdict(lambda : {})
+        for cause in causes:
+            for p_type in p_types:
+                err[cause][p_type] = rrmse(exact[cause][p_type][0], approx[cause][p_type][0], exact[cause][p_type][1], approx[cause][p_type][1])
+                err2[cause][p_type] = rmse(exact[cause][p_type][0], approx[cause][p_type][0], exact[cause][p_type][1], approx[cause][p_type][1])
+
+        nruns = len(inf.models)
+
+        msg = f"[{nruns} runs\t T_learn={tlearn:.0f} ms. \t T_infer={tinfer:.0f} ms."
+        log.info(msg)
+
+        # Store the results
+        r = pd.DataFrame(dict(modelname=modelname, method=method, cause=cause, effect=effect, tol=tol,
+                        num_runs_param=num_runs, max_iter_param=max_iter, seed=seed, tlearn=tlearn, tinfer=tinfer, remove_outliers=remove_outliers,
+                        datasize=len(data),
+                        exact = json.dumps(exact), approx = json.dumps(approx),
+                        rrmse = json.dumps(err), rmse = json.dumps(err2), nruns=nruns
+                        ), index=[0])
+        results = pd.concat([results, r], ignore_index=True)
+        t0 = Watch.get_time()
+
+    # Check if the directory for the file at resfilepath exists, and create it if it does not
+    if not resfilepath.parent.exists():
+        resfilepath.parent.mkdir(parents=True, exist_ok=True) 
+    # Save results   
+    results.to_csv(resfilepath)
+
+def process_parameters_wrapper(params):
+    # Define the logger
+    log = get_clogger(__name__, fmt=log_format, filename=logfile)
+    try:
+        process_parameters(params, log)
+    except Exception as e:
+        log.exception(e)
+
+def generate_parameter_combinations(modelpath):
+    # Generate combinations for each method 
+    parameter_combinations = []
+    for seed, remove_outliers in itertools.product(seed_values, remove_outliers_values):
+        for method in method_values:
+            if method == "EMCC":
+                for max_iter in max_iter_values_emcc:
+                    parameter_combinations.append((num_runs, modelpath, resfolder, run_step, seed, remove_outliers, method, max_iter, None))
+            elif method == "GDCC":
+                for tol in tol_values_gdcc:
+                    parameter_combinations.append((num_runs, modelpath, resfolder, run_step, seed, remove_outliers, method, None, tol))
+    return parameter_combinations
+
+
+
+if __name__ == "__main__":
+    # Display the number of available worker processes
+    log = get_clogger(__name__, fmt=log_format, filename=logfile)
+    available_workers = cpu_count()
+    log.info(f"Number of available workers: {available_workers}")
+    modelpaths = glob.glob(os.path.join('./papers/gradient_journal/models/synthetic/simple/', '*.uai'))
+    n = len(modelpaths)
+    for i, modelpath in enumerate(modelpaths):
+        # e.g. modelpath = "./papers/gradient_journal/models/synthetic/s123/random_mc2_n5_mid3_d1000_05_mr098_r10_8.uai"
+
+        # TODO:
+        modelpath = "./papers/gradient_journal/models/synthetic/simple/simple_nparents2_nzr02_zdr05_2.uai"
+
+        model_name = os.path.basename(modelpath) # e.g. model_name = 'random_mc2_n5_mid3_d1000_05_mr098_r10_8.uai'
+        if 0: #len(glob.glob(os.path.join(resfolder, model_name.replace('.', '_') + '*'))) == 18: # have we already computed all the results for the model?
+            log.info(f'Skipping {model_name} ({i} out of {n-1}). It has been done before. ')
+            continue
+        else: # the results are not yet computed
+            log.info(f'Processing {model_name} ({i} out of {n-1}) ...')
+            parameter_combinations = generate_parameter_combinations(modelpath)
+            if 1: # set to True to test in non-parallel settings
+                log.info(parameter_combinations[0])
+                process_parameters_wrapper(parameter_combinations[0])
+                quit()  
+            else:
+                # Parallel approach
+                with Pool() as pool:
+                    pool.map(process_parameters_wrapper, parameter_combinations)
+
+
+
+
+
