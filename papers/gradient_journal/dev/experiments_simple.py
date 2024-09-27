3c995913066ebdb68713648f2d17dab4847be333
Jan Górecki <gorecki@opf.slu.cz>
2024-09-27 12:14:00 +0200

    simple exps according to Rafa

--- papers/gradient_journal/dev/experiments_simple.py
+++ papers/gradient_journal/dev/experiments_simple.py
@@ -4,7 +4,6 @@ from multiprocessing import Pool, cpu_count
 import os
 import glob
 from collections import defaultdict
-import json
 
 from concurrent_log_handler import ConcurrentRotatingFileHandler
 #pip install concurrent-log-handler
@@ -28,7 +27,8 @@ from bcause.util.watch import Watch
 num_runs = 100
 run_step = 5
 resfolder = "./papers/gradient_journal/results/synthetic/simple/"
-rewrite = True
+REWRITE = False
+RUN_IN_PARALLEL = True
 
 # Multi parameters
 USE_FULL_PARAMETERS = True
@@ -72,6 +72,14 @@ def get_clogger(logname, level=logging.INFO, stream=sys.stdout, fmt=None, filena
 
 def process_parameters(params, log):
     num_runs, modelpath, resfolder, run_step, seed, remove_outliers, method, max_iter, tol = params
+
+    # Check that the experiments with these parameters have not been run yet
+    modelname = os.path.basename(modelpath).split(".")[0]
+    resfilepath = Path(resfolder, f"{modelname}_uai_{method}_x{num_runs}_iter{max_iter}_tol{tol}_ro{remove_outliers}_s{seed}.csv")    
+    if resfilepath.exists() and not REWRITE:
+        log.info(f'{resfilepath} already exists. Skipping ...')
+        return
+
     # Processing logic here
     print(f"Processing: {seed=}, {remove_outliers=}, {method=}, {max_iter=}, {tol=}") # Single parameters are omitted here
 
@@ -104,24 +112,15 @@ def process_parameters(params, log):
     causes = [f'V{i}' for i in range(1, nV + 1)] # V1, V2, V3
     effect = 'V0'
 
-    p_types = ['PS', 'PN'] # 'PNS'
     exact = defaultdict(lambda : {})
-    for cause in causes:
-        for p_type in p_types:
-            interval = (info_query[(info_query['cause'] == cause) & (info_query['query'] == p_type)]['low'].values[0],
-                        info_query[(info_query['cause'] == cause) & (info_query['query'] == p_type)]['upp'].values[0])
-            exact[cause][p_type] = interval
-   
-    modelname = os.path.basename(modelpath).split(".")[0]
-
+    for query in ['PS', 'PN']: # 'PNS'
+        for cause in causes:  
+            interval = (info_query[(info_query['cause'] == cause) & (info_query['query'] == query)]['low'].values[0],
+                        info_query[(info_query['cause'] == cause) & (info_query['query'] == query)]['upp'].values[0])
+            exact[query][cause] = interval
 
     # Set the results
-    resfilepath = Path(resfolder, f"{modelname}_uai_{method}_x{num_runs}_iter{max_iter}_tol{tol}_ro{remove_outliers}_s{seed}.csv")
     results = pd.DataFrame()
-
-    # Check that the experiments with these parameters have not been run yet
-    # if resfilepath.exists() and not rewrite:
-    #     return
 
     # Determine the method
     if method == "GDCC":
@@ -130,6 +129,13 @@ def process_parameters(params, log):
         inf = EMCC(model, data, num_runs=num_runs, max_iter=max_iter, outliers_removal=remove_outliers)
     else:
         raise ValueError("Wrong learning method")
+    
+    # Queries and inference functions
+    queries_f = { 
+        'PNS': inf.prob_necessity_sufficiency,
+        'PS' : inf.prob_sufficiency,
+        'PN' : inf.prob_necessity
+    }
 
     ### Start processing ###
     tlearn = 0
@@ -139,45 +145,43 @@ def process_parameters(params, log):
     # Learning loop
     for _ in inf.compile_incremental(run_step): # The learning is done here at each iteration
 
-        t1 = Watch.get_time()
-        # Run the queries
-        approx = defaultdict(lambda : {})
-        for cause in causes:
-            for p_type in p_types:
-                if p_type == 'PNS':
-                    p = inf.prob_necessity_sufficiency(cause, effect, true_false_cause=(0,1), true_false_effect=(0,1))
-                elif p_type == 'PS':
-                    p = inf.prob_sufficiency(cause, effect, true_false_cause=(0,1), true_false_effect=(0,1))
-                elif p_type == 'PN':
-                    p = inf.prob_necessity(cause, effect, true_false_cause=(0,1), true_false_effect=(0,1))
-                approx[cause][p_type] = p
-        t2 = Watch.get_time()
-
-        tlearn = tlearn + t1-t0 
-        tinfer = t2-t1
-        # NOTE: When `compile_incremental()` is called, learning occurs only for the new iterations. 
-        #       However, when `inf.prob_necessity_sufficiency` is called, inference is indeed performed again with all the learned models.
-
-        err, err2 = defaultdict(lambda : {}), defaultdict(lambda : {})
-        for cause in causes:
-            for p_type in p_types:
-                err[cause][p_type] = rrmse(exact[cause][p_type][0], approx[cause][p_type][0], exact[cause][p_type][1], approx[cause][p_type][1])
-                err2[cause][p_type] = rmse(exact[cause][p_type][0], approx[cause][p_type][0], exact[cause][p_type][1], approx[cause][p_type][1])
-
         nruns = len(inf.models)
 
-        msg = f"[{nruns} runs\t T_learn={tlearn:.0f} ms. \t T_infer={tinfer:.0f} ms."
-        log.info(msg)
+        t1 = Watch.get_time()
+        tlearn = tlearn + t1-t0 
 
-        # Store the results
-        r = pd.DataFrame(dict(modelname=modelname, method=method, cause=cause, effect=effect, tol=tol,
-                        num_runs_param=num_runs, max_iter_param=max_iter, seed=seed, tlearn=tlearn, tinfer=tinfer, remove_outliers=remove_outliers,
-                        datasize=len(data),
-                        exact = json.dumps(exact), approx = json.dumps(approx),
-                        rrmse = json.dumps(err), rmse = json.dumps(err2), nruns=nruns
-                        ), index=[0])
-        results = pd.concat([results, r], ignore_index=True)
+        # NOTE: When `compile_incremental()` is called, learning occurs only for the new iterations. 
+        # However, when `inf.prob_necessity_sufficiency` is called, inference is indeed performed again with all the learned models.
+
+        # Run the queries
+        for query, infer_f in queries_f.items():
+            if query not in exact:
+                continue
+            for cause in causes:
+                exact_low = exact[query][cause][0]
+                exact_upp = exact[query][cause][1]
+
+                t2 = Watch.get_time()     
+                approx_low, approx_upp = infer_f(cause, effect, true_false_cause=(0,1), true_false_effect=(0,1))
+                tinfer = Watch.get_time()-t2 # TODO: Rafa, check if the times are computed correctly
+
+                err = rrmse(exact_low, approx_low, exact_upp, approx_upp)
+                err2 = rmse(exact_low, approx_low, exact_upp, approx_upp)
+
+                # Store the results
+                r = pd.DataFrame(dict(modelname=modelname, method=method, query = query, cause=cause, effect=effect, tol=tol,
+                                num_runs_param=num_runs, max_iter_param=max_iter, seed=seed, tlearn=tlearn, tinfer=tinfer, remove_outliers=remove_outliers,
+                                datasize=len(data),
+                                exact_low = exact_low, exact_upp = exact_upp,
+                                approx_low = approx_low, approx_upp = approx_upp,
+                                rrmse = err, rmse = err2, nruns=nruns
+                                ), index=[0])
+                results = pd.concat([results, r], ignore_index=True)
+
+        msg = f"[{nruns} runs\t T_learn={tlearn:.0f} ms."
+        log.info(msg)
         t0 = Watch.get_time()
+    # End Learning loop
 
     # Check if the directory for the file at resfilepath exists, and create it if it does not
     if not resfilepath.parent.exists():
@@ -218,24 +222,20 @@ if __name__ == "__main__":
     for i, modelpath in enumerate(modelpaths):
         # e.g. modelpath = "./papers/gradient_journal/models/synthetic/s123/random_mc2_n5_mid3_d1000_05_mr098_r10_8.uai"
 
-        # TODO:
+        # TODO: the line below is just for debug, comment for production
         modelpath = "./papers/gradient_journal/models/synthetic/simple/simple_nparents2_nzr02_zdr05_2.uai"
 
         model_name = os.path.basename(modelpath) # e.g. model_name = 'random_mc2_n5_mid3_d1000_05_mr098_r10_8.uai'
-        if 0: #len(glob.glob(os.path.join(resfolder, model_name.replace('.', '_') + '*'))) == 18: # have we already computed all the results for the model?
-            log.info(f'Skipping {model_name} ({i} out of {n-1}). It has been done before. ')
-            continue
-        else: # the results are not yet computed
-            log.info(f'Processing {model_name} ({i} out of {n-1}) ...')
-            parameter_combinations = generate_parameter_combinations(modelpath)
-            if 1: # set to True to test in non-parallel settings
-                log.info(parameter_combinations[0])
-                process_parameters_wrapper(parameter_combinations[0])
-                quit()  
-            else:
-                # Parallel approach
-                with Pool() as pool:
-                    pool.map(process_parameters_wrapper, parameter_combinations)
+        log.info(f'Processing {model_name} ({i} out of {n-1}) ...')
+        parameter_combinations = generate_parameter_combinations(modelpath)
+        if not RUN_IN_PARALLEL: # set to True to test in non-parallel settings
+            log.info(parameter_combinations[0])
+            process_parameters_wrapper(parameter_combinations[1])
+            quit()  
+        else:
+            # Parallel approach
+            with Pool() as pool:
+                pool.map(process_parameters_wrapper, parameter_combinations)
 
 
 
